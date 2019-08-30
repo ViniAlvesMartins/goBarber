a68a18b5bf4cd8ec83fb5068f48c07fe817d8137
Vinicius Martins <vinicius.martins52@etec.sp.gov.br>
2019-08-30 00:33:18 -0300

    Configuração do sequelize, e criptografia de senha

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.status(200).json({ message: 'hello World' });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
