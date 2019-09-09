442ba427e8044b5b7c54b0a4d37ecb2337b11c59
Vinicius Martins <vinicius.martins52@etec.sp.gov.br>
2019-09-09 20:48:49 -0300

    Criação de um agendamento

--- src/database/migrations/20190906003418-add-avatar-field-to-users.js
+++ src/database/migrations/20190906003418-add-avatar-field-to-users.js
@@ -1,13 +1,5 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-
     return queryInterface.addColumn('users', 'avatar_id', {
       type: Sequelize.INTEGER,
       references: { model: 'files', key: 'id' },
